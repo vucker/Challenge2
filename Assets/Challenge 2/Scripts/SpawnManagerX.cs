bc239055fc0fc252f4828107f5747a9a75567743
vucker <103271312+vucker@users.noreply.github.com>
2026-02-28 22:44:09 +0300

    Реализован случайный интервал спавна
    Time.time

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,16 +11,25 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
-
-    // Start is called before the first frame update
-    void Start()
+    private float minSpawnInterval = 3.0f;
+    private float maxSpawnInterval = 5.0f;
+    private float intervalTime = 0f;
+    private float nextTimerSpawn;
+    private void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        nextTimerSpawn = Time.time + Random.Range(minSpawnInterval, maxSpawnInterval);
+    }
+    void Update()
+    {
+        if (Time.time >= nextTimerSpawn)
+        {
+            SpawnRandomBall();
+            nextTimerSpawn = Time.time + Random.Range(minSpawnInterval, maxSpawnInterval);
+        }
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
@@ -29,5 +38,4 @@ public class SpawnManagerX : MonoBehaviour
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
-
 }
