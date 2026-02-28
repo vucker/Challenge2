a3903cad012e318fd9ec4289aef5d0859a936a50
vucker <103271312+vucker@users.noreply.github.com>
2026-02-28 22:10:58 +0300

    Собаки и мячи уничтожаются

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
+    private float leftLimit = -30;
     private float bottomLimit = -5;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
+            Debug.Log("Собака убежала");
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        else if (transform.position.y < bottomLimit)
         {
+            Debug.Log("Мяч лопнул");
             Destroy(gameObject);
         }
 
