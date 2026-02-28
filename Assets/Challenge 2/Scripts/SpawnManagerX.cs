438f6c7c8672e0e95541639aaa9971cefd0f0e93
vucker <103271312+vucker@users.noreply.github.com>
2026-02-28 22:45:14 +0300

    Удалены неиспользуемые переменные

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,10 +10,8 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
     private float minSpawnInterval = 3.0f;
     private float maxSpawnInterval = 5.0f;
-    private float intervalTime = 0f;
     private float nextTimerSpawn;
     private void Start()
     {
